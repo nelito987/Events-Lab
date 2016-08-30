8591dad44f5267e11b19b23b310187b949b66086
nelito987 <nelito987@abv.bg>
2016-08-30 18:34:34 +0300

    initial commit

---
+++ Events.Data/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Events.Data
+{
+    public class Comment
+    {
+        public Comment()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string AuthorId { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+
+        public int EventId { get; set; }
+
+        [Required]
+        public virtual Event Event { get; set; }
+    }
+}
